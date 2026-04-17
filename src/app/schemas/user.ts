3b155ef8b9d4ae7228ea09d4f9b3ca2f75032eac
guiottijoao <joaovitorguiotti8@gmail.com>
2026-04-17 17:23:02 -0300

    FEAT: Create user schema for fields validation with zod

---
+++ src/app/schemas/user.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const createUserSchema = z.object({
+  name: z.string(),
+  email: z.email(),
+  password: z.string().min(6),
+});
+
+export const updateUserSchema = createUserSchema.partial();
