aaa04ed3321885202db95d117416ff0d9c7bb324
guiottijoao <joaovitorguiotti8@gmail.com>
2026-04-20 11:14:02 -0300

    FIX: Correct status code that it sent by error handler middleware

--- src/app/middleware/error-handler.ts
+++ src/app/middleware/error-handler.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 
-export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-  console.error(err.stack)
-  res.status(500).json({ message: err.message || 'Internal Server Error' })
+export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+  const status = err.status || 500
+  res.status(status).json({ message: err.message || 'Internal Server Error' })
 }
