67c9138177656e43aa18232a62d08d2bda3eba91
guiottijoao <joaovitorguiotti8@gmail.com>
2026-07-10 11:40:56 -0300

    FEAT: Create general sync endpoint to orchestrate  accounts and transactions sync in one endpoint

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import authRoutes from "./auth";
 import userRoutes from "./users";
 import accountRoutes from "./accounts";
 import transactionRoutes from "./transaction";
+import generalSyncRoutes from "./general-sync";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/accounts", accountRoutes);
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/transactions", transactionRoutes);
+router.use("/general-sync", generalSyncRoutes);
 
 export default router;
