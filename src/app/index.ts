18dcdbc20e4558c1f8ee356e3746f40dac686730
guiottijoao <joaovitorguiotti8@gmail.com>
2026-07-15 17:18:55 -0300

    FEAT: Improve authentication with cookies httpOnly

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,17 +1,26 @@
-import '../config/zod'
+import "../config/zod";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
-import swaggerUi from 'swagger-ui-express'
+import swaggerUi from "swagger-ui-express";
 import routes from "./routes/index";
 import { errorHandler } from "./middleware/error-handler";
-import { generateOpenApiDocument } from '../config/openapi';
+import { generateOpenApiDocument } from "../config/openapi";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api", routes);
 app.use(errorHandler);
 
-const document = generateOpenApiDocument()
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(document))
+const document = generateOpenApiDocument();
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(document));
 
 export default app;
